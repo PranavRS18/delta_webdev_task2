97e9f570e3ea5f90c61b8c9bcf4fe31b1ec53449
PranavRS18 <pranavsaravanan.r@gmail.com>
2025-06-08 15:56:35 +0530

    Added v0.1

--- script.js
+++ script.js
@@ -1,177 +1,153 @@
 // DOM
 const remInPx = parseFloat(getComputedStyle(document.documentElement).fontSize);
-const world = document.getElementById("world");
-const ctx = world.getContext("2d");
+const world = document.querySelector("#world")
+const worldCtx = world.getContext("2d");
+
+const city = document.querySelector("#city");
+const cityCtx = city.getContext("2d");
 
 world.width = world.clientWidth;
 world.height = world.clientHeight;
 
-ctx.strokeStyle = "limegreen";
-ctx.lineWidth = 0.15 * remInPx;
+city.width = world.clientWidth;
+city.height = world.clientHeight;
 
-const blockWidth = world.width / 5;
-const blockHeight = world.height / 16;
+const nColumns = 12;
+const nRows = 12;
+const blockWidth = world.width / nColumns;
+const blockHeight = world.height / nRows;
 
-for (let col = 0; col < 5; col++) {
-    ctx.beginPath();
-    ctx.moveTo(col * blockWidth, 0);
-    ctx.lineTo(col * blockWidth, world.height);
-    ctx.stroke();
-    ctx.closePath();
+for (let col = 0; col < nColumns; col++) {
+    worldCtx.beginPath();
+    worldCtx.strokeStyle = "limegreen";
+    worldCtx.lineWidth = 0.075 * remInPx;
+    worldCtx.moveTo(col * blockWidth, 0);
+    worldCtx.lineTo(col * blockWidth, world.height);
+    worldCtx.stroke();
+    worldCtx.closePath();
 }
 
-for (let row = 0; row < 16; row++) {
-    ctx.beginPath();
-    ctx.moveTo(0, row * blockHeight);
-    ctx.lineTo(world.width, row * blockHeight);
-    ctx.stroke();
-    ctx.closePath();
+for (let row = 0; row < nRows; row++) {
+    worldCtx.beginPath();
+    worldCtx.strokeStyle = "limegreen";
+    worldCtx.lineWidth = 0.075 * remInPx;
+    worldCtx.moveTo(0, row * blockHeight);
+    worldCtx.lineTo(world.width, row * blockHeight);
+    worldCtx.stroke();
+    worldCtx.closePath();
 }
 
 let rectX, rectY;
 const rectWidth = blockWidth * 4 / 5;
 const rectHeight = blockHeight * 4 / 5;
 
-let buildingX, buildingY, nBuildings, shooterBuilding, shooterX, shooterY, shooterAngle;
-
+let buildingX, buildingY, nBuildings;
 const buildingWidth = rectWidth / 3;
 const buildingHeight = rectHeight / 3;
-let blockBuildings = [];
 let buildings = [];
-let shooters = [];
-let shooterBuildings = [];
-let shooterRadius = 0.5 * remInPx;
-const shooterRange = 12 * remInPx;
 
-let nKeys, keyX, keyY;
+let shooterX, shooterY, shooterBuilding;
+let shooterRadius = buildingWidth / 15;
+let shooters = [];
+const shooterRange = shooterRadius * 24;
+const shooterArc = Math.PI * 9 / 32;
+
+let keyX, keyY, nKeys;
+let keyRadius = buildingWidth / 15;
 let keys = [];
-const keyRadius = 0.5 * remInPx;
 
 // Buildings, Grass and Keys
-for (let col = 0; col < 5; col++) {
-    for (let row = 0; row < 16; row++) {
+for (let col = 0; col < nColumns; col++) {
+    for (let row = 0; row < nRows; row++) {
+        rectX = blockWidth * (col + 1 / 10) ;
+        rectY = blockHeight * (row + 1 / 10) ;
 
-        rectX = col * world.width / 5 + (rectWidth / 8);
-        rectY = row * world.height / 16 + (rectHeight / 8);
+        worldCtx.beginPath();
+        worldCtx.fillStyle = "limegreen";
+        worldCtx.fillRect(rectX, rectY, rectWidth, rectHeight);
+        worldCtx.closePath();
 
-        // Grass
-        ctx.beginPath();
-        ctx.fillStyle = "limegreen";
-        ctx.fillRect(rectX, rectY, rectWidth, rectHeight);
-        ctx.closePath();
+        let blockBuildings = [];
+        nBuildings = 2 + Math.floor(Math.random() * 2);
 
-        blockBuildings = [];
-
-        // Buildings
-        nBuildings = Math.round(2 + Math.random());
-        for (let building = 0; building < nBuildings; building++) {
-            buildingX = rectX + Math.random() * rectWidth * 2 / 3;
-            buildingY = rectY + Math.random() * rectHeight * 2 / 3;
-
-            ctx.beginPath();
-            ctx.fillStyle = "black";
-            ctx.fillRect(buildingX, buildingY, buildingWidth, buildingHeight);
-            ctx.closePath();
-
-            buildings.push([buildingX, buildingY]);
+        for(let building = 0; building < nBuildings; building++) {
+            buildingX = rectX + Math.floor(Math.random() * (rectWidth - buildingWidth));
+            buildingY = rectY + Math.floor(Math.random() * (rectHeight - buildingHeight));
             blockBuildings.push([buildingX, buildingY]);
+            buildings.push([buildingX, buildingY]);
         }
 
         shooterBuilding = blockBuildings[Math.floor(Math.random() * blockBuildings.length)];
-        shooterBuildings.push(shooterBuilding);
-
         let [shooterBuildingX, shooterBuildingY] = shooterBuilding;
-        shooterX = shooterBuildingX + shooterRadius + Math.random() * (buildingWidth - 2 * shooterRadius);
-        shooterY = shooterBuildingY + shooterRadius + Math.random() * (buildingHeight - 2 * shooterRadius);
 
-        ctx.beginPath();
-        ctx.fillStyle = "red";
-        ctx.arc(shooterX, shooterY, shooterRadius, 0, Math.PI * 2, false);
-        ctx.fill();
-        ctx.closePath();
-
+        shooterX = shooterBuildingX + Math.floor(Math.random() * (buildingWidth - shooterRadius));
+        shooterY = shooterBuildingY + Math.floor(Math.random() * (buildingHeight - shooterRadius));
         shooters.push([shooterX, shooterY, Math.random() * Math.PI * 2]);
 
-        nKeys = 2 + Math.random() * 3;
+        nKeys = 3 + Math.random() * 3;
+
         for (let key = 0; key < nKeys; key++) {
-
-            let keyX = rectX + Math.random() * rectWidth;
-            let keyY = rectY + Math.random() * rectHeight;
-            keys.push([keyX, keyY])
+            keyX = (col * blockWidth) + Math.floor(Math.random() * blockWidth);
+            keyY = (row * blockHeight) + Math.floor(Math.random() * blockHeight);
+            keys.push([keyX, keyY]);
         }
-
     }
 }
 
-// Game
-const game = document.getElementById("game");
-context = game.getContext("2d");
+// Draw Buildings, Keys and Shooters
+function drawCanvas() {
+    buildings.forEach(building => {
+        let [buildingNewX, buildingNewY] = building;
 
-game.width = world.clientWidth;
-game.height = world.clientHeight;
-
-const playerRadius = 1.2 * remInPx;
-const playerSpeed = 0.6 * remInPx;
-let playerX = playerRadius;
-let playerY = playerRadius;
-
-// Animation
-function animate() {
-    context.clearRect(0, 0, world.clientWidth, world.clientHeight);
-
-    // Player
-    context.beginPath();
-    context.fillStyle = "blue";
-    context.arc(playerX, playerY, playerRadius, 0, Math.PI * 2, false);
-    context.fill();
-    context.closePath();
-
-    window.addEventListener("keydown", (event) => {
-        if (event.key === "s") {
-            playerY += playerSpeed;
-        } else if (event.key === "w") {
-            playerY -= playerSpeed;
-        } else if (event.key === "d") {
-            playerX += playerSpeed;
-        } else if (event.key === "a") {
-            playerX -= playerSpeed;
-        }
-    });
-
-    // Shooters
-    shooters.forEach((shooter, idx, shooters) => {
-
-        let [newShooterX, newShooterY, shooterAngle] = shooter;
-        context.strokeStyle = "red";
-        context.fillStyle = "rgba(255, 0, 0, 0.3)"
-
-        context.beginPath();
-        context.moveTo(newShooterX, newShooterY);
-        context.arc(newShooterX, newShooterY, shooterRange, shooterAngle, shooterAngle + (Math.PI * 9 / 16), false);
-        context.lineTo(newShooterX, newShooterY);
-        context.fill();
-        context.stroke();
-        context.closePath()
-
-        shooters[idx] = [newShooterX, newShooterY, shooterAngle + (Math.PI / 50)];
-
-    });
-
-    // Keys
-    keys.forEach((key) => {
-
-        [keyX, keyY] = key
-
-        context.beginPath();
-        context.fillStyle = "yellow";
-        context.arc(keyX, keyY, keyRadius, 0, Math.PI * 2, false);
-        context.fill();
-        context.closePath();
+        cityCtx.beginPath();
+        cityCtx.fillStyle = "black";
+        cityCtx.fillRect(buildingNewX, buildingNewY, buildingWidth, buildingHeight);
+        cityCtx.closePath();
 
     })
 
+    shooters.forEach(shooter => {
+
+        let [shooterNewX, shooterNewY] = shooter;
+
+        cityCtx.beginPath();
+        cityCtx.fillStyle = "red";
+        cityCtx.arc(shooterNewX, shooterNewY, shooterRadius, 0, Math.PI * 2, false);
+        cityCtx.fill();
+        cityCtx.closePath();
+    })
+
+    keys.forEach(key => {
+
+        let [keyNewX, keyNewY] = key;
+
+        cityCtx.beginPath();
+        cityCtx.fillStyle = "yellow";
+        cityCtx.arc(keyNewX, keyNewY, keyRadius, 0, Math.PI * 2, false);
+        cityCtx.fill();
+        cityCtx.closePath();
+    })
+}
+
+function animate() {
+    cityCtx.clearRect(0, 0, world.clientWidth, world.clientHeight);
+    drawCanvas();
+    shooters.forEach((shooter, idx, shooters) => {
+        [shooterNewX, shooterNewY, shooterNewAngle] = shooter;
+
+        cityCtx.beginPath();
+        cityCtx.strokeStyle = "red";
+        cityCtx.fillStyle = "rgba(255, 0, 0, 0.3";
+        cityCtx.moveTo(shooterNewX, shooterNewY);
+        cityCtx.arc(shooterNewX, shooterNewY, shooterRange,
+            shooterNewAngle - shooterArc, shooterNewAngle + shooterArc, false);
+        cityCtx.lineTo(shooterNewX, shooterNewY);
+        cityCtx.stroke();
+        cityCtx.fill();
+        cityCtx.closePath();
+
+        shooters[idx] = [shooterNewX, shooterNewY, shooterNewAngle + (Math.PI / 100)]
+    });
     requestAnimationFrame(animate);
 }
 requestAnimationFrame(animate);
-
-
